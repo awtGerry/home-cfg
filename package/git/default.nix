a6bb845b298735f93ac046c6041d2259e9f42cae
Victor Rodriguez <awtgerry@gmail.com>
2023-12-23 23:38:27 -0600

    Add git config

---
+++ package/git/default.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Victor Rodriguez";
+    userEmail = "awtGerry@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "master";
+      pull.rebase = "merges";
+      rebase.autostash = true;
+      diff.algorithm = "histogram";
+
+      ignores = [
+        ".test"
+        ".envrc"
+        ".project"
+        ".settings"
+      ];
+
+    }; # extraConfig
+
+    home.packages = with pkgs; [
+      gitAndTools.git-bug
+    ];
+
+  };
+}
