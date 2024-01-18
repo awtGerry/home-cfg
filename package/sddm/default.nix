1121a85fb8d4269354c5a9d23cef454c131283d6
Victor Rodriguez <awtgerry@gmail.com>
2024-01-17 20:30:58 -0600

    sddm change to sugar-candy (DONE)

--- package/sddm/default.nix
+++ package/sddm/default.nix
@@ -2,21 +2,20 @@
 
 let
   imgUrl = "https://raw.githubusercontent.com/awtGerry/home-cfg/master/bg.jpg";
+  themeUrl = "https://raw.githubusercontent.com/awtGerry/home-cfg/master/package/sddm/theme.conf";
 
   image = pkgs.fetchurl {
     url = imgUrl;
     sha256 = "sha256-2FMvWIjeTTtSmVzjQdBh6ImLVrJ6raSQaV+VvGiUNuQ=";
   };
+
+  theme = pkgs.fetchurl {
+    url = themeUrl;
+    sha256 = "sha256-yRyVrpAqPhUGIeajiUPeGD2DJO/4ZWJ2lKqQkYB9aiI=";
+  };
 in
 pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "MarianArlt";
-  #   repo = "sddm-sugar-dark";
-  #   rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-  #   sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
-  # };
-
   src = pkgs.fetchFromGitHub {
     owner = "Kangie";
     repo = "sddm-sugar-candy";
@@ -28,7 +27,8 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out
     cp -R ./* $out/
     cd $out
-    rm Backgrounds/Mountain.jpg
-    cp -r ${image} Backgrounds/Mountain.jpg
+    rm theme.conf
+    cp -r ${image} Backgrounds/bg.jpg
+    cp -r ${theme} theme.conf
   '';
 }
