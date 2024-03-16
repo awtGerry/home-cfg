5542100f5a87fd334f4b3b23c7d1442345ab62d3
Victor Rodriguez <awtgerry@gmail.com>
2024-03-15 22:18:51 -0600

    hyprland detected by sddm
    Actually stable version with the tags working.

--- package/hyprland/default.nix
+++ package/hyprland/default.nix
@@ -2,44 +2,29 @@
 
 {
   wayland.windowManager.hyprland.settings = {
-    general = {
-      monitor = [
-        "DisplayPort-0,1920x1080@144,0x0,0"
-      ];
-    };
-
-    decoration = {
-      shadow_offset = "0 5";
-      "col.shadow" = "rgba(00000099)";
-    };
-
-
     "$mod" = "SUPER";
-
-    bind = [
-      # mouse movements
-      "$mod, mouse:272, movewindow"
-      "$mod, mouse:273, resizewindow"
-      "$mod ALT, mouse:272, resizewindow"
-
-      "$mod, Return, exec, wezterm"
-      "$mod, T, exec, wezterm"
-      "$mod, W, exec, firefox"
-      "$mod CTRL, q, exec, hyprctl dispatch exit"
-    ]
-    ++ (
-      builtins.concatLists (builtins.genList (
-        x: let
-          ws = let
-            c = (x + 1) / 10;
-          in
-            builtins.toString (x + 1 - (c * 10));
-        in [
-          "$mod ${ws}, workspace ${toString (x + 1)}"
-          "$mod SHIFT ${ws}, movetoworkspace ${toString (x + 1)}"
-        ]
-      )
-      10)
-    );
+    bind =
+      [
+        "$mod, W, exec, firefox"
+        "$mod, Return, exec, wezterm"
+        "$mod, T, exec, kitty"
+        ", Print, exec, grimblast copy area"
+      ]
+      ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
   };
 }
