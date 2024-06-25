2d0adcba46b5e331ab6d2955bd17c1c7fbec2373
Victor Rodriguez <awtGerry@gmail.com>
2024-06-24 22:32:23 -0600

    drives

--- hosts/lyra/hardware/drive.nix
+++ hosts/lyra/hardware/drive.nix
@@ -2,18 +2,14 @@
 
 {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a13b729b-bf7a-425b-b5ba-772a3cc5a14a";
+    { device = "/dev/disk/by-uuid/d8136752-a39b-42d1-9b86-a2258d80d615";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D70B-965B";
+    { device = "/dev/disk/by-uuid/681E-1C3B";
       fsType = "vfat";
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/5eb70bcf-39f7-4caf-992e-dc1e19ced93b";
-      fsType = "ext4";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   fileSystems."/media/Drive" =
@@ -21,7 +17,7 @@
       fsType = "ext4";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/cca4bde4-ea4c-4901-a794-2905e004f950"; }
-    ];
+  #swapDevices =
+  #  [ { device = "/dev/disk/by-uuid/cca4bde4-ea4c-4901-a794-2905e004f950"; }
+  #  ];
 }
