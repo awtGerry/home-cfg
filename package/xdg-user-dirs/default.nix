619d252596c09d78f04c756b8561f1bfaaa56c0f
Victor Rodriguez <awtGerry@gmail.com>
2024-07-08 14:01:23 -0600

    add user directories

--- package/xdg-user-dirs/default.nix
+++ package/xdg-user-dirs/default.nix
@@ -1,10 +1,20 @@
 { lib, pkgs, config, ... }:
 
 let
-  cfg = config.xdg.userDirs;
+  home = config.home.homeDirectory;
 in
 {
   programs.xdg.userDirs = {
     enable = true;
+    createDirectories = true; # Create directories if they don't exist
+
+    desktop = "${home}/Desktop";
+    documents = "${home}/Documents";
+    downloads = "${home}/Downloads";
+    music = "${home}/Music";
+    pictures = "${home}/Pictures";
+    public = "${home}/Public";
+    templates = "${home}/Templates";
+    videos = "${home}/Videos";
   };
 }
