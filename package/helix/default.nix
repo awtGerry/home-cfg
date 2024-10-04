ac772ef255e9ab396214f76d31c9d48b62aaa8bd
Victor Rodriguez <awtGerry@gmail.com>
2024-10-03 20:16:55 -0600

    update: Add lsp langs to helix
    Not done yet.

--- package/helix/default.nix
+++ package/helix/default.nix
@@ -16,7 +16,9 @@
           "keep_primary_selection"
           "collapse_selection"
         ];
+        # Until I get used to gl gh (it does makes more sense)
         "$" = "goto_line_end";
+        "V" = "extend_line_below"; # TODO: Check what 'V' is used for on helix.
         "0" = "goto_line_start";
       };
       keys.insert = {
@@ -46,20 +48,73 @@
               command = [ "${fmt}/bin/nixfmt" ];
             };
           };
+
+        typescript-language-server = {
+          command = "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server";
+          args = [
+            "--stdio"
+            "--tsserver-path=${pkgs.nodePackages.typescript}/lib/node_modules/typescript/lib"
+          ];
+        };
       };
 
-      language = [
-        {
-          name = "nix";
-          language-servers = [ "nil" ];
-          formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
-          auto-format = true;
-        }
-        {
-          name = "rust";
-          auto-format = true;
-        }
-      ];
+      language =
+        let
+          jsts-ls = [
+            {
+              name = "typescript-language-server";
+              except-features = [ "format" ];
+            }
+          ];
+          vscode-lsp = [ { name = "vscode-langservers-extracted"; } ];
+        in
+        [
+          {
+            name = "nix";
+            language-servers = [ "nil" ];
+            formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+            auto-format = true;
+          }
+          {
+            name = "rust";
+            auto-format = true;
+          }
+          {
+            name = "c";
+            auto-format = false; # I only want to format on C when i tell helix to do it.
+          }
+          {
+            name = "bash";
+            auto-format = true;
+            file-types = [
+              "sh"
+              "bash"
+            ];
+            formatter = {
+              command = "${pkgs.shfmt}/bin/shfmt";
+            };
+          }
+          {
+            name = "typescript";
+            auto-format = true;
+            language-servers = jsts-ls;
+          }
+          {
+            name = "javascript";
+            auto-format = true;
+            language-servers = jsts-ls;
+          }
+          {
+            name = "html";
+            auto-format = true;
+            language-servers = vscode-lsp;
+            file-types = [
+              "html"
+              "css"
+              "json"
+            ];
+          }
+        ];
     };
   };
 }
---
+++ package/wezterm/default.nix
@@ -0,0 +1,12 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  programs.wezterm = {
+    enable = true;
+  };
+}
