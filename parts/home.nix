0902c71e806f880b982f2e8ce0a2445455c3193b
Victor Rodriguez <awtGerry@gmail.com>
2024-10-21 17:19:01 -0600

    feat: parts configurations

---
+++ parts/home.nix
@@ -0,0 +1,132 @@
+{
+  self,
+  inputs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.awt.homeConfigurations;
+  enabledConfigs = lib.filterAttrs (_: config: config.enable) cfg;
+  configs = builtins.mapAttrs (_: config: config.finalHome) enabledConfigs;
+  packages = builtins.attrValues (builtins.mapAttrs (_: config: config.packageModule) enabledConfigs);
+in
+{
+  _file = ./home.nix;
+
+  options = {
+    awt.homeConfigurations = lib.mkOption {
+      type = lib.types.attrsOf (
+        lib.types.submodule (
+          { name, config, ... }:
+          {
+            options = {
+              enable = lib.mkEnableOption "Activar esta configuracion." // {
+                default = true;
+              };
+
+              nixpkgs = lib.mkOption {
+                type = lib.types.uspecified;
+                default = inputs.nixpkgs;
+              };
+
+              system = lib.mkOption {
+                type = lib.types.enum [
+                  "x86_64-linux"
+                  "aarch64-linux"
+                  "x86_64-darwin"
+                ];
+              };
+
+              username = lib.mkOption {
+                type = lib.types.str;
+                default = builtins.elemAt (lib.strings.split "@" name) 0;
+              };
+
+              hostname = lib.mkOption {
+                type = lib.types.str;
+                default = builtins.elemAt (lib.strings.split "@" name) 2;
+              };
+
+              entryPoint = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              base = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              homeDirectory = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              modules = lib.mkOption {
+                type = lib.types.listOf lib.types.unspecified;
+                default = [ ];
+              };
+
+              finalModules = lib.mkOption {
+                type = lib.types.listOf lib.types.unspecified;
+                readOnly = true;
+              };
+
+              packageName = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              finalPackage = lib.mkOption {
+                type = lib.types.package;
+                readOnly = true;
+              };
+
+              finalHome = lib.mkOption {
+                type = lib.types.unspecified;
+                readOnly = true;
+              };
+
+              packageModule = lib.mkOption {
+                type = lib.types.unspecified;
+                readOnly = true;
+              };
+            };
+
+            config = lib.mkIf config.enable {
+              # Example: gerry_artemis
+              entryPoint = import "${self}/home/configurations/${config.username}_${config.hostname}.nix";
+            };
+
+            homeDirectory = "${config.base}/${config.username}";
+
+            finalModules = [
+              config.entryPoint
+              {
+                home = {
+                  inherit (config) username homeDirectory;
+                };
+              }
+            ] ++ config.modules ++ builtins.attrValues self.homeModules;
+
+            packageName = "home/config/${name}";
+            finalPackage = config.finalHome.activationPackage;
+
+            packageModule = {
+              ${config.system}.${config.packageName} = config.finalPackage;
+            };
+
+            finalHome = inputs.home-manager.lib.homeManagerConfiguration {
+              pkgs = config.nixpkgs.legacyPackages.${config.system};
+              modules = config.finalModules;
+            };
+          }
+        )
+      );
+    };
+  };
+
+  config.flake.homeConfigurations = configs;
+  config.flake.packages = lib.mkMerge packages;
+}
---
+++ parts/system.nix
@@ -0,0 +1,117 @@
+{
+  self,
+  inputs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.awt.nixosConfigurations;
+  configs = builtins.mapAttrs (_: config: config.finalSystem) cfg;
+  packages = builtins.attrValues (builtins.mapAttrs (_: config: config.packageModule) cfg);
+in
+{
+  _file = "./system.nix";
+
+  options = {
+    # Atributos para la configuracion de nixos
+    awt.nixosConfigurations = lib.mkOptions {
+      type = lib.types.attrsOf (
+        lib.types.submodule (
+          { name, config, ... }:
+          {
+            # Submodulos
+            options = {
+              # Cual nixpkgs se usara
+              nixpkgs = lib.mkOption {
+                type = lib.types.unspecified;
+                default = inputs.nixpkgs;
+              };
+              # Arquitectura del sistema
+              system = lib.mkOption {
+                type = lib.types.enum [
+                  "x86_64-linux"
+                  "aarch64-linux"
+                ];
+              };
+
+              # Modulos adicionales
+              modules = lib.mkOption {
+                type = lib.types.listOf lib.types.unspecified;
+                default = [ ];
+              };
+
+              # Archivos de solo lectura para valores derivados
+              configFolder = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              entryPoint = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              bootloader = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              hardware = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              finalModules = lib.mkOption {
+                type = lib.types.listOf lib.types.uspecified;
+                readOnly = true;
+              };
+
+              packageName = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+
+              finalPackage = lib.mkOption {
+                type = lib.types.str;
+                readOnly = true;
+              };
+            };
+
+            config = {
+              configFolder = "${self}/nixos/configurations";
+              entryPoint = import "${config.configFolder}/${name}.nix" (inputs // { inherit self; });
+              bootloader = "${config.configFolder}/bootloader/${name}.nix";
+              hardware = "${config.configFolder}/hardware/${name}.nix";
+
+              finalModules =
+                [
+                  { boot.tmp.cleanOnBoot = true; }
+                  { networking.hostName = name; }
+                  { nix.flakes.enable = true; }
+                  { documentation.man.enable = true; }
+                  { documentation.man.generateCaches = true; }
+                  { nixpkgs.hostPlataform.system = config.system; }
+                ]
+                ++ config.modules
+                ++ builtins.attrValues { inherit (config) entryPoint bootloader hardware; }
+                ++ builtins.attrValues self.nixosModules
+                ++ builtins.attrValues self.mixedModules;
+
+              packageName = "nixos/config/${name}";
+              finalPackage = config.finalSystem.config.system.build.toplevel;
+              packageModule = {
+                ${config.system}.${config.packageName} = config.finalPackage;
+              };
+
+              finalSystem = config.nixpkgs.lib.nixosSystem { modules = config.finalModules; };
+            };
+          }
+        )
+      );
+    };
+  };
+
+  config.flake.nixosConfigurations = configs;
+  config.flake.packages = lib.mkMerge packages;
+}
