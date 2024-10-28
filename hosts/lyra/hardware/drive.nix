8474bb42f50f1ad1e7ff09a4ee7f7195ef87af7c
Victor Rodriguez <awtGerry@gmail.com>
2024-10-28 14:32:53 -0600

    update: update disk

--- hosts/lyra/hardware/drive.nix
+++ hosts/lyra/hardware/drive.nix
@@ -1,21 +1,30 @@
-{ config, lib, pkgs, modulesPath, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d8136752-a39b-42d1-9b86-a2258d80d615";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/d8136752-a39b-42d1-9b86-a2258d80d615";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/681E-1C3B";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/681E-1C3B";
+    fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
 
-  fileSystems."/media/Drive" =
-    { device = "/dev/disk/by-uuid/6af823f1-8303-4819-84a0-54143b00b04c";
-      fsType = "ext4";
-    };
+  fileSystems."/media/Drive" = {
+    device = "/dev/disk/by-label/Unix";
+    fsType = "exfat";
+  };
 
   #swapDevices =
   #  [ { device = "/dev/disk/by-uuid/cca4bde4-ea4c-4901-a794-2905e004f950"; }
