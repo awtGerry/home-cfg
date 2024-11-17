359a8667c0175985531eb52ccc5f9552703a6caf
Victor Rodriguez <awtGerry@gmail.com>
2024-11-16 18:59:24 -0600

    hyprland configs

--- package/hyprland/default.nix
+++ package/hyprland/default.nix
@@ -1,10 +1,16 @@
-{ config, lib, pkgs, inputs, ... }: 
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 
 let
   startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
-      ${pkgs.waybar}/bin/waybar &
-      ${pkgs.swww}/bin/swww init &
-      "random-wp" &
+    ${pkgs.waybar}/bin/waybar &
+    ${pkgs.swww}/bin/swww init &
+    "random-wp" &
   '';
 in
 {
@@ -20,8 +26,24 @@ in
     # general settings
     general = {
       monitor = [
-        "DP-1,1920x1080@144,0x0,1"
-        "HDMI-A-1,disable"
+        "DP-1,1920x1080@144,1920x0,1" # Right of hdmi
+        "HDMI-A-1,1920x1080@60,0x0,1"
+
+        # Only 1 monitor
+        # "DP-1,1920x1080@144,0x0,1"
+        # "HDMI-A-1,disable"
+      ];
+      # Workspace for monitors
+      workspace = [
+        "1, monitor:DP-1"
+        "2, monitor:DP-1"
+        "3, monitor:DP-1"
+        "4, monitor:DP-1"
+        "5, monitor:DP-1"
+        "6, monitor:DP-1"
+        "7, monitor:HDMI-A-1"
+        "8, monitor:DP-1"
+        "9, monitor:DP-1"
       ];
       gaps_in = 5;
       gaps_out = 5;
