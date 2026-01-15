5d57a9306244d47078da83214fe266b636a97bb3
Victor Rodriguez <awtGerry@gmail.com>
2026-01-15 13:50:37 -0600

    update: Add athena config
    vm using nixos

--- home/configurations/gerry_athena.nix
+++ home/configurations/gerry_athena.nix
@@ -8,21 +8,14 @@
     ];
 
     theme = {
-      variant = "dark";
-      baseScheme = "ayu";
+      variant = "light";
+      baseScheme = "e-ink";
     };
 
     apps = {
       browser = "firefox";
       terminal = "st";
       editor = "helix";
-    };
-
-    home.sessionVariables = {
-      # Usa Wayland para aplicaciones Chrome & Electron
-      NIXOS_OZONE_WL = 1;
-      # Mejora la apariencia de aplicaciones de Java
-      JDK_JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
     };
 
     dconf.enable = true;
